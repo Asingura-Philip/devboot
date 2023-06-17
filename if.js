de21752dcd0445f97d79ecfa9310ddd00f353026
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-17 21:34:52 +0300

    if statements js

---
+++ if.js
@@ -0,0 +1,21 @@
+/*
+We need to keep mature movies away from children! I guess it's the law or something.
+Write two if/else blocks - one for Bobby and one for Jill. 
+If Bobby is 18 or older, print Bobby is old enough, 
+otherwise print Bobby is not old enough.
+ Do the same for "Jill". 
+*/
+const bobbyAge = 17
+const jillAge = 19
+
+// don't touch above this line
+if (bobbyAge > 18) {
+  console.log("Bobby is old enough")
+}else{
+  console.log("Bobby is not old enough")
+}
+if (jillAge > 18) {
+  console.log("Jill is old enough")
+} else {
+  console.log("Jill is not old enough")
+}
