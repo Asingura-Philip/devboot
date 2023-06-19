649b762829ab27f074735dd37d06d21c092127a4
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-19 06:05:58 +0300

    a function

---
+++ boot-functions.js
@@ -0,0 +1,36 @@
+/*Complete the getLabel function. It takes the number of stars a
+movie received in its reviews and returns a string label describing 
+how good the movie was.
+
+8 - 10 stars: great
+4 - 7 stars: okay
+less than or equal to 3 stars: awful*/
+//let numStars;
+function getLabel(numStars) {
+    if(numStars >= 8){
+        return "great";
+    }if(numStars >= 4){
+        return "okay";
+    }else{
+        return "awful";
+    }
+  }
+  
+  // don't touch below this line
+  
+  function test(numStars) {
+    console.log(`Stars=${numStars}, The movie was ${getLabel(numStars)}`)
+  }
+  
+  test(10)
+  test(9)
+  test(8)
+  test(7)
+  test(6)
+  test(5)
+  test(4)
+  test(3)
+  test(2)
+  test(1)
+  test(0)
+  
